986741ef169b75cb8eec34e4d314f5d3382c3c79
rgliever <rgliever@ucsc.edu>
2014-05-14 21:19:04 -0700

    Added small things to slug_mem.c

--- src/slug_mem.c
+++ src/slug_mem.c
@@ -7,15 +7,38 @@
 
 struct node {
 	size_t len;
-	unsigned addr;
+	void addr;
 	time_t timestap;
 	char *location;
-	int free;
+	int is_free;
 	node *next;
+};
+
+struct malloc_list {
+	node head;
+	node tail;
+	node curr; /* can be used for traversing the list in for loops */
+};
+
+/* 
+Allocates memory by calling malloc.
+Returns address of allocated memory.
+Records address, length, current timestamp, and location of the call in an
+internal data structure.
+WHERE is a string constant that records the filename and line number of caller.
+*/
+void *slug_malloc ( size_t size, char *WHERE )
+{
+	/* if (size <= 0) print to stderr, don't exit
+	if( size > 128 MB) print to stderr, exit the program */
+	unsigned request = malloc(sizeof(size));
+	return request;
 }
 
-void *slug_malloc ( size_t size, char *WHERE );
+void slug_free ( void *addr, char *WHERE )
+{
+}
 
-void slug_free ( void *addr, char *WHERE );
-
-void slug_memstats ( void );
+void slug_memstats ( void ) 
+{
+}
