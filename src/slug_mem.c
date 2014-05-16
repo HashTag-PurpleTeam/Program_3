a6ede25675ff209cb4f0237eb95322f1411d1896
rgliever <rgliever@ucsc.edu>
2014-05-15 17:03:11 -0700

    Filled in slug_malloc & added other minor things

--- src/slug_mem.c
+++ src/slug_mem.c
@@ -4,12 +4,16 @@
  */
 
 #include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
 
 struct node {
-	size_t len;
+	size_t size;
 	void addr;
-	time_t timestap;
-	char *location;
+	time_t alloc_time;
+	time_t free_time;
+	char *alloc_location;
+	char *free_location;
 	int is_free;
 	node *next;
 };
@@ -20,6 +24,8 @@ struct malloc_list {
 	node curr; /* can be used for traversing the list in for loops */
 };
 
+struct malloc_list m_list = malloc(sizeof(struct malloc_list));
+
 /* 
 Allocates memory by calling malloc.
 Returns address of allocated memory.
@@ -29,10 +35,28 @@ WHERE is a string constant that records the filename and line number of caller.
 */
 void *slug_malloc ( size_t size, char *WHERE )
 {
-	/* if (size <= 0) print to stderr, don't exit
-	if( size > 128 MB) print to stderr, exit the program */
-	unsigned request = malloc(sizeof(size));
-	return request;
+	if(size <= 0) {
+		fprintf(stderr, "%s\n", strerror(errno));
+	}
+	
+	if(size > 134217728) { /* i.e. 128 MB */
+		fprintf(stderr, "%s\n", strerror(errno));
+		exit(1); /*terminate program*/
+	}
+	
+	struct node tmp = malloc(sizeof(struct node));
+	tmp->size = size;
+	void *address = tmp->addr = malloc(sizeof(size));
+	tmp->alloc_time = time(NULL);
+	tmp->free_time = NULL;
+	tmp->alloc_location = WHERE;
+	tmp->free_location = NULL;
+	tmp->is_free = 0;
+	tmp->next = NULL;
+	m_list->tail->next = tmp;
+	m_list->tail = m_list->tail->next;
+	
+	return address;
 }
 
 void slug_free ( void *addr, char *WHERE )
@@ -41,4 +65,16 @@ void slug_free ( void *addr, char *WHERE )
 
 void slug_memstats ( void ) 
 {
+}
+
+void alloc_new_node( size_t size )
+{
+	struct node tmp = malloc(sizeof(node));
+	tmp->size = size;
+	tmp->addr = malloc(sizeof(size));
+	tmp->alloc_time = time(NULL);
+	tmp->free_time = NULL;
+	tmp->location = 
+	m_list->tail->next = tmp;
+	m_list->tail = m_list->tail->next;
 }
--- src/slug_mem.h
+++ src/slug_mem.h
@@ -10,10 +10,11 @@
 
 /* LM: The following defines were recommended in the assignment
  *     specs. */
-#define malloc(s) slug_malloc((s), FILE_POS)
 
 #define FUNCTIONIZE(a,b) a(b)
 #define STRINGIZE(a) #a
 #define INT2STRING(i) FUNCTIONIZE(STRINGIZE,i)
 #define FILE_POS __FILE__ ":" INT2STRING(__LINE__)
+
+#define malloc(s) slug_malloc((s), FILE_POS)
 
